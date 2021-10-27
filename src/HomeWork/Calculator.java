6a62bbb001c9c0aab622c3383aca0a6f32d10185
Al2206 <hr297@365mspro.com>
2021-10-28 00:42:56 +0300

    Initial commit

---
+++ src/HomeWork/Calculator.java
@@ -0,0 +1,38 @@
+// Написать программу реализующую следующие задачи: сложение; вычитание; умножение; деление; сравнение.
+// Программа должна быть реализована с помощью оператора switch
+package HomeWork;
+
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        double a = 0;
+        double b = 0;
+        char op;
+        double result = 0.0;
+        Scanner scanObject = new Scanner(System.in);
+        System.out.println(" Введите первое число :");
+        a = scanObject.nextInt();
+        System.out.println(" Введите оператор :");
+        op = scanObject.next() .charAt(0);
+        System.out.println(" Введите второе число :");
+        b = scanObject.nextInt();
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            default:
+                System.out.println("Введено неверное значение оператора");
+        }
+        System.out.println( a + " " + op + " " + b + " = " + result);
+    }
+}
+//Для сравнения нужно ещё один switch запускать?
